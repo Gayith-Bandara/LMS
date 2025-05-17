ef3f8810619b5e1681651a3b326f78c39400eddc
gayith <gayith.mxz@gmail.com>
2025-05-17 16:04:31 +0530

    Completed home page

--- client/src/components/student/Companies.jsx
+++ client/src/components/student/Companies.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {assets} from '../../assets/assets'
 
 const Companies = () =>{
     return (
-        <div>
-            <h1>Companies</h1>
+        <div className='pt-16'>
+            <p className='text-base text-gray-500'>Trusted by learners from</p>
+            <div className='flex flex-wrap items-center justify-center md:gap-16 gap-6 md:mt-10 mt-5'>
+                <img src={assets.microsoft_logo} alt="microsoft_logo" className='md:w-28 w-20'/>
+                <img src={assets.walmart_logo} alt="walmart_logo" className='md:w-28 w-20'/>
+                <img src={assets.accenture_logo} alt="accenture_logo" className='md:w-28 w-20'/>
+                <img src={assets.adobe_logo} alt="adobe_logo" className='md:w-28 w-20'/>
+                <img src={assets.paypal_logo} alt="paypal_logo" className='md:w-28 w-20'/>
+                
+            </div>
         </div>
     )
     
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,10 +1,41 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
     
-    const value = {}
+    const currency = import.meta.env.VITE_CURRENCY
+
+    const navigate = useNavigate();
+
+    const [allCourses, setAllCourses] = useState([]);//use state hook 
+    const [isEducator, setIsEducator] = useState(true);//use state hook 
+
+    //set all courses
+    const fetchAllCourses = async ()=>{
+        setAllCourses(dummyCourses);
+    }
+
+    //call the fetchAllCourses funtion(everytime this project renders this useEffect react method will be called)
+    useEffect(()=>{
+        fetchAllCourses(), []
+    })
+
+    //function to calculate average rating of course
+    const calculateRating = (course) =>{
+        if(course.courseRatings.length === 0){return 0;}
+        let totalRating = 0;
+        course.courseRatings.forEach((rating => {
+            totalRating += rating.rating;
+        }))
+        return totalRating / course.courseRatings.length;
+    }
+
+    const value = {
+        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator
+    }
 
     return (
         <AppContext.Provider value={value}>
